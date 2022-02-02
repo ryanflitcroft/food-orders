16d73e5b4c503b4a72eacf6d8e3d8a76ca313eb8
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-01 16:32:29 -0800

    assets stored in public/assets. state set for entreeId, sideId, drinkId, orderName, instructions in App. Function setup for body components Header, Main, Footer and called in App.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 function App() {
+  const [entreeId, setEntreeId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [orderName, setOrderName] = useState('');
+  const [instructions, setInstructions] = useState([]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+
   );
 }
 
